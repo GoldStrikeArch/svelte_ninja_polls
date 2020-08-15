05240d5a276b479d7edb3238d502c8ac33c4f02e
Mihail Pertsev <arthasmontano@mail.ru>
2020-08-15 16:20:02 +0300

    1

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  let count = 0;
+  function handleClick() {
+    count++;
+  }
+
+  let disabled = false;
+</script>
+
+<style>
+  .disabled {
+    background: gray;
+  }
+</style>
+
+<main class="bg-red-700">
+  <button
+    class="font-bold py-2 px-4 rounded bg-blue-400"
+    on:click={handleClick}>
+    Click
+  </button>
+  <p>{count}</p>
+</main>
